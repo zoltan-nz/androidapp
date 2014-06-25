2112f5be659cb9e2f27f912a25535482ffa04064
Zoltan <zdebre@gmail.com>
2014-06-25 23:13:10 +1200

    New Activity works and compiled

--- app/src/main/java/szines/androidapp/DisplayMessageActivity.java
+++ app/src/main/java/szines/androidapp/DisplayMessageActivity.java
@@ -1,17 +1,29 @@
 package szines.androidapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
-import szines.androidapp.R;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(Home.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
+
     }
 
     @Override
--- app/src/main/java/szines/androidapp/Home.java
+++ app/src/main/java/szines/androidapp/Home.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.view.View;
 
 
 public class Home extends Activity {
