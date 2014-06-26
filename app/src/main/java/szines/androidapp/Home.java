a5206ed296664de2c7c7c8b3220475c538bd5a52
Zoltan <zdebre@gmail.com>
2014-06-26 23:25:21 +1200

    Add Android 2.1 support

--- app/src/main/java/szines/androidapp/Home.java
+++ app/src/main/java/szines/androidapp/Home.java
@@ -1,13 +1,12 @@
 package szines.androidapp;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
 import android.view.View;
+import android.widget.EditText;
 
 
 public class Home extends Activity {
