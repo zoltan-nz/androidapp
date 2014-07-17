62d925215df1c3b20c1ee452984a42f159ca95df
Zoltan <zdebre@gmail.com>
2014-07-17 23:29:14 +1200

    Writing more test

--- app/src/main/java/szines/androidapp/MainActivity.java
+++ app/src/main/java/szines/androidapp/MainActivity.java
@@ -1,6 +1,5 @@
 package szines.androidapp;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -17,7 +16,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_home);
+        setContentView(R.layout.fragment_main);
     }
 
 
@@ -25,7 +24,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.home, menu);
+        inflater.inflate(R.menu.main, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
