0e744fdddf3f0d5d3cccd0074d9057e4a0c57f20
Zoltan <zdebre@gmail.com>
2014-07-18 23:45:42 +1200

    Change method to public

--- app/src/main/java/szines/androidapp/MainActivity.java
+++ app/src/main/java/szines/androidapp/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
-    private void sendMessage(View view) {
+    public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
