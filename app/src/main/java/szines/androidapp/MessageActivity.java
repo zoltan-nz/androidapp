678add2d856b7a080380bf7067c44f03b7b2ab3f
Zoltan <zdebre@gmail.com>
2014-07-21 23:55:31 +1200

    More fragement

--- app/src/main/java/szines/androidapp/MessageActivity.java
+++ app/src/main/java/szines/androidapp/MessageActivity.java
@@ -3,6 +3,10 @@ package szines.androidapp;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class MessageActivity extends Fragment {
@@ -28,10 +32,6 @@ public class MessageActivity extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.article_view, container, false);
+        return inflater.inflate(R.layout.fragment_message, container, false);
     }
-
-
-
-
 }
