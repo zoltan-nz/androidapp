e82ed1d045b4b91b2298b4e11d08ac07da71bcbb
Zoltan <zdebre@gmail.com>
2014-07-23 23:50:41 +1200

    Clean up the project

--- app/src/main/java/szines/androidapp/MainActivity.java
+++ app/src/main/java/szines/androidapp/MainActivity.java
@@ -1,98 +1,13 @@
 package szines.androidapp;
 
-import android.content.Intent;
+
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 
+public class MainActivity extends Activity {
 
-public class MainActivity extends FragmentActivity {
-    public final static String EXTRA_MESSAGE = "szines.androidapp.MESSAGE";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the HomeActivity/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        switch (id) {
-            case R.id.action_search:
-                openSearch();
-                return true;
-            case R.id.action_settings:
-                openSettings();
-                return true;
-            case R.id.action_webview:
-                openWebView();
-                return true;
-            case R.id.action_button:
-                openButton();
-                return true;
-            case R.id.action_login:
-                openLogin();
-                return true;
-            case R.id.action_plusbase:
-                openPlusBase();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, MessageFragment.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-
-    private void openSearch() {
-
-    }
-
-    private void openSettings() {
-        Intent intent = new Intent(this, SettingsActivity.class);
-        startActivity(intent);
-    }
-
-    private void openWebView() {
-        Intent intent = new Intent(this, WebViewActivity.class);
-        startActivity(intent);
-    }
-
-    private void openButton() {
-        Intent intent = new Intent(this, ButtonActivity.class);
-        startActivity(intent);
-    }
-
-    private void openLogin() {
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-    }
-
-    private void openPlusBase() {
-        Intent intent = new Intent(this, PlusBaseActivity.class);
-        startActivity(intent);
     }
 }
