51377b8c4a9529965d10bcb4de9d21445ac488ae
Zoltan <zdebre@gmail.com>
2014-07-24 08:50:57 +1200

    Add GcmTest

--- app/src/main/java/szines/androidapp/MainActivity.java
+++ app/src/main/java/szines/androidapp/MainActivity.java
@@ -3,11 +3,13 @@ package szines.androidapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Hello", "launched");
     }
 }
